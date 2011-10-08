b8f5592a9efe65d98b462acda30825893d1c1b19
John Postlethwait <john.postlethwait@gmail.com>
2011-10-08 02:19:23 -0700

    Logging a scrape error and some UI nicities.

--- headphones/librarysync.py
+++ headphones/librarysync.py
@@ -47,8 +47,9 @@ def libraryScan(dir=None):
         # Try to read the metadata
         try:
           f = MediaFile(song)
-
         except:
+          e = sys.exc_info()[1]
+          logger.error('Got Exception: ' + str(e))
           logger.error('Cannot read file: ' + file)
           continue
 
